3abfa073da28dcaedb50942dd62f117058b08635
Diddi Oscarsson <diddi.oscarsson@cyberbacon.net>
2013-10-30 11:08:03 +0100

    Add initial create_resources

--- manifests/init.pp
+++ manifests/init.pp
@@ -12,7 +12,33 @@
 #   include 'cron'
 #   class { 'cron': }
 
-class cron {
+class cron(
+  $job    => 'NONE',
+  $daily  => 'NONE',
+  $weekly => 'NONE',
+  $monthly => 'NONE',
+  hourly   => 'NONE',
+) {
   include cron::install
+
+  if $job != 'NONE' {
+    create_resources('cron::job', $job)
+  }
+
+  if $daily != 'NONE' {
+    create_resources('cron::daily', $daily)
+  }
+
+  if $weekly != 'NONE' {
+    create_resources('cron::weekly', $weekly)
+  }
+
+  if $monthly != 'NONE' {
+    create_resources('cron::monthly', $monthly)
+  }
+
+  if $hourly != 'NONE' {
+    create_resources('cron::hourly', $hourly)
+  }
 }
 
