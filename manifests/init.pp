a1c05a34173dcb1a5eb6e8cc423c04ac077d81db
Diddi Oscarsson <diddi.oscarsson@cyberbacon.net>
2013-10-30 16:11:29 +0100

    Add support for cron::daily etc...

--- manifests/init.pp
+++ manifests/init.pp
@@ -16,23 +16,27 @@ class cron {
   include cron::install
 $network_if_statics = hiera_hash('network::if::static',undef)
 
-  $job = hiera_hash('cron::job',undef)
+  $job = hiera_hash('cron::job', undef)
   if $job != undef {
     create_resources('cron::job', $job)
   }
 
+  $daily = hiera_hash('cron::daily', undef)
   if $daily != undef {
     create_resources('cron::daily', $daily)
   }
 
+  $weekly = hiera_hash('cron::weekly', undef)
   if $weekly != undef {
     create_resources('cron::weekly', $weekly)
   }
 
+  $monthly = hiera_hash('cron::monthly', undef)
   if $monthly != undef {
     create_resources('cron::monthly', $monthly)
   }
 
+  $hourly = hiera_hash('cron::hourly', undef)
   if $hourly != undef {
     create_resources('cron::hourly', $hourly)
   }
